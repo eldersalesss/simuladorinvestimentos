a2d70daca77a06b64114a250f6124f094d6a7ddf
Elder <Elder@Elder-PC>
2017-04-20 02:04:48 -0300

    Commit teste (removendo comentários).

--- test/Testes/testeLCIeLCA.java
+++ test/Testes/testeLCIeLCA.java
@@ -1,11 +1,5 @@
 package Testes;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 import org.junit.Test;
 import static org.junit.Assert.*;
