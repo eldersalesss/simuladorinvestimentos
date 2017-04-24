7ff9236ebd798a7af3f4d9d7ede0f00d6d27e4db
LosMendoncas <2141178589@C2a40brj504jz97.acad.unit.br>
2017-04-24 17:59:01 -0300

    atualização do teste unitário CDBeLC

--- test/Testes/testeCDBeLC.java
+++ test/Testes/testeCDBeLC.java
@@ -14,7 +14,7 @@ public class testeCDBeLC {
 
     @Test
     public void testCDB() {
-        int tempo = 1; // meses
+        int tempo = 1; // meses //
 //        int tempo = 2; // meses
         Double valor = 5000.0;
         Double retorno = 5044.24;
@@ -25,7 +25,7 @@ public class testeCDBeLC {
 
     @Test
     public void testLC() {
-        int tempo = 1; // meses
+        int tempo = 1; // meses // 
 //        int tempo = 2 // meses
         Double valor = 5000.0;
         Double retorno = 5043.50;
