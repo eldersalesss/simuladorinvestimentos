48f26cb35e14564ed0e2ec971ad336eac7cdd6dd
thauanlima <thauan1997@hotmail.com>
2017-04-24 21:08:50 -0300

    Update testeLCIeLCA.java

--- test/Testes/testeLCIeLCA.java
+++ test/Testes/testeLCIeLCA.java
@@ -16,10 +16,21 @@ public class testeLCIeLCA {
     @Test
     public void testLCA(){
         int tempo = 1;
+        // taxa = 0,008912
         Double valor = 5000.0;
         Double retorno = 5044.56;
-        LCIeLCA t1 = new LCIeLCA (valor, tempo);
-        assertEquals(retorno,t1.simuladorLCA());
+        LCIeLCA l1 = new LCIeLCA (valor, tempo);
+        assertEquals(retorno,l1.simuladorLCA());
+    }
+    
+     @Test
+    public void testLCI(){
+        int tempo = 1;
+        // taxa = 0,009202
+        Double valor = 5000.0;
+        Double retorno = 5046.01;
+        LCIeLCA l2 = new LCIeLCA (valor, tempo);
+        assertEquals(retorno,l2.simuladorLCI());
     }
 
 }
