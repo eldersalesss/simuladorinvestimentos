3e2070339ee1a1623a70df311b46efa3f804d05a
LosMendoncas <2141178589@C2a40brj504jz97.acad.unit.br>
2017-05-02 15:39:45 -0300

    Refatorando a classe LCIeLCA

--- src/java/Sistema/LCIeLCA.java
+++ src/java/Sistema/LCIeLCA.java
@@ -1,65 +1,58 @@
-
-package javaapplication1;
+package Sistema;
 
 /**
  *
  * @author Thauan
  */
-public class LCIeLCA extends Investimentos{
+public class LCIeLCA extends Investimentos {
+
     /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+     */
     private Double taxaLCI;//0.9202%
     private Double taxaLCA; //0.8912;%
-    private String tipo;
-    private double valor;
-    private int tempo;
-    
-    
+
     public LCIeLCA(Double valor, int tempo) {
         this.valor = valor;
         this.tempo = tempo;
-        this.tipo = tipo;
     }
 
     @Override
     public Double previsaoRetorno() {
-            if (tipo.equalsIgnoreCase("LCI")) return simuladorLCI();
-            else if (tipo.equalsIgnoreCase("LCA")) return simuladorLCA();
-            else{
-                return null;
-            }
+        if (tipo.equalsIgnoreCase("LCI")) {
+            return simuladorLCI();
+        } else if (tipo.equalsIgnoreCase("LCA")) {
+            return simuladorLCA();
+        } else {
+            return null;
+        }
     }
 
-
-    public Double simuladorLCI(){
+    public Double simuladorLCI() {
         Double retorno = this.valor;
 
-        for(int i=0;i < tempo;i++){
+        for (int i = 0; i < tempo; i++) {
             retorno += ((retorno * taxaLCI) / 100);
         }
 
         return retorno;
     }
 
-    public Double simuladorLCA(){
+    public Double simuladorLCA() {
         Double retorno = this.valor;
 
-        for(int i=0;i < tempo;i++){
+        for (int i = 0; i < tempo; i++) {
             retorno += ((retorno * taxaLCA) / 100);
         }
 
         return retorno;
     }
 
-
-
-    public void setTipo(String tipo){
+    public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-
 
     public Double getValor() {
         return this.valor;
@@ -85,6 +78,4 @@ public class LCIeLCA extends Investimentos{
         this.taxaLCA = taxaLCA;
     }
 
-
-}
 }
