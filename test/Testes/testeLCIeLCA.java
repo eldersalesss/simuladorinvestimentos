18854dc9b2ee76f1832dffae4901743ff3174fee
GabrielRMendonca <gabriel.rmendonca@souunit.com.br>
2017-05-02 23:47:37 -0300

    Update testeLCIeLCA.java

--- test/Testes/testeLCIeLCA.java
+++ test/Testes/testeLCIeLCA.java
@@ -1,7 +1,7 @@
 package Testes;
 
 
-import Classes.LCIeLCA;
+import Sistema.LCIeLCA;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -21,6 +21,7 @@ public class testeLCIeLCA {
         Double valor = 5000.0;
         Double retorno = 5044.56;
         LCIeLCA l1 = new LCIeLCA (valor, tempo);
+        l1.setTaxaLCA(0.8912);
         assertEquals(retorno,l1.simuladorLCA());
     }
     
@@ -31,6 +32,7 @@ public class testeLCIeLCA {
         Double valor = 5000.0;
         Double retorno = 5046.01;
         LCIeLCA l2 = new LCIeLCA (valor, tempo);
+        l2.setTaxaLCI(0.9202);
         assertEquals(retorno,l2.simuladorLCI());
     }
 
