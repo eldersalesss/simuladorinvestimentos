e4a5f34874a99accdce0fa3ca4e7056666b0a4c1
GabrielRMendonca <gabriel.rmendonca@souunit.com.br>
2017-05-02 23:45:43 -0300

    Update testeCDBeLC.java

--- test/Testes/testeCDBeLC.java
+++ test/Testes/testeCDBeLC.java
@@ -16,11 +16,9 @@ public class testeCDBeLC {
 
     @Test
     public void testCDB() {
-        int tempo = 1; // meses 
-//        int tempo = 2; // meses 
+        int tempo = 1; // meses  
         Double valor = 5000.0;
         Double retorno = 5044.24;
-//        Double retorno = 5088.99;
         CDBeLC t1 = new CDBeLC(valor, tempo);
         t1.setTaxaCDB(0.8848);
         assertEquals(retorno, t1.simuladorCDB());
@@ -30,10 +28,8 @@ public class testeCDBeLC {
     @Test
     public void testLC() {
         int tempo = 1; // meses
-//        int tempo = 2 // meses
         Double valor = 5000.0;
         Double retorno = 5043.50;
-//        Double retorno = 5087.48;
         CDBeLC t2 = new CDBeLC(valor, tempo);
         t2.setTaxaLC(0.87);
         assertEquals(retorno, t2.simuladorLC());
