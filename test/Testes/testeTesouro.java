a35647552b93fcee624b0f85fdedc7135dbd08f3
adrianewey <adrian.newey@souunit.com.br>
2017-05-08 16:00:06 -0300

    Correção testes mensais

--- test/Testes/testeTesouro.java
+++ test/Testes/testeTesouro.java
@@ -59,29 +59,33 @@ public class testeTesouro {
         TesouroDireto tesouro = new TesouroDireto(valor, tempo);
         tesouro.setTaxaSELIC(0.7442);
         DecimalFormat formato = new DecimalFormat("#.##");
-        Assert.assertEquals(retorno, Double.valueOf(formato.format(tesouro.simulador_MensalSelic())));
+        Assert.assertEquals(retorno, Double.valueOf(formato.format(tesouro.simulador_MensalSelic()).replace(',','.')));
     }
 
     // Mudei o nome do método porque estava igual ao do primeiro método do IPCA
     @Test
     public void testIPCAMensal() {
-        int tempo = 6;
+        int tempo = 3;
         Double valor = 5000.0; //Inserido mensalmente durante o periodo de tempo. 6 meses = 6 vezes.
-        Double retorno = 30655.67;
-
+        Double retorno = 15185.62;
+        
         TesouroDireto tesouro = new TesouroDireto(valor, tempo);
-        Assert.assertEquals(retorno, tesouro.simulador_MensalIPCA());
+        tesouro.setTaxaIPCA(0.6162);
+        DecimalFormat formato = new DecimalFormat("#.##");
+        Assert.assertEquals(retorno, Double.valueOf(formato.format(tesouro.simulador_MensalIPCA()).replace(',','.')));
     }
 
     // Mudei o nome do método porque estava igual ao do primeiro método do Prefixo
     @Test
     public void testPrefixadoMensal() {
-        int tempo = 6;
+        int tempo = 3;
         Double valor = 5000.0; //Inserido mensalmente durante o periodo de tempo. 6 meses = 6 vezes.
-        Double retorno = 30674.17;
+        Double retorno = 15190.82;
 
         TesouroDireto tesouro = new TesouroDireto(valor, tempo);
-        Assert.assertEquals(retorno, tesouro.simulador_MensalPrefixado());
+        tesouro.setTaxaIPCA(0.6334);
+        DecimalFormat formato = new DecimalFormat("#.##");
+        Assert.assertEquals(retorno, Double.valueOf(formato.format(tesouro.simulador_MensalIPCA()).replace(',','.')));
     }
 
 }
