901205a10cb5f6e3ce513d646d042b7518c0b6d8
Elder <Elder@Elder-PC>
2017-05-16 16:51:07 -0300

    UPDATE - correções de importações

--- test/Testes/testeInvestimento.java
+++ test/Testes/testeInvestimento.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test.Testes;
+package Testes;
 
+import src.java.Sistema.LCIeLCA;
 import src.java.Sistema.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
--- test/Testes/testeLCIeLCA.java
+++ test/Testes/testeLCIeLCA.java
@@ -1,4 +1,4 @@
-package test.Testes;
+package Testes;
 
 
 import src.java.Sistema.LCIeLCA;
